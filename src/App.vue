4d861795446902354d7bad19be33ff26a2f085c3
Nick <931239318@qq.com>
2023-09-13 13:22:06 +0800

    09-12 update.

--- src/App.vue
+++ src/App.vue
@@ -14,15 +14,15 @@
             <el-input placeholder="请输入文本" v-model="input_text" class="input-with-select">
             </el-input>
           </el-col>
-          <el-col :span="4"><el-button type="primary" @click="start">开始捏脸</el-button>
+          <el-col :span="4"><el-button type="primary" @click="inputClick">开始捏脸</el-button>
           </el-col>
         </el-row>
-        <el-row id="iter">迭代次数：<el-input-number v-model="num" @change="handleChange" :min="1" :max="200"
+        <el-row id="iter">迭代次数：<el-input-number v-model="iter_num" @change="handleChange" :min="1" :max="200"
             label="迭代轮数"></el-input-number>
         </el-row>
         <el-divider></el-divider>
         <h2>捏脸结果</h2>
-        <el-row :gutter="0"><el-col :span="12"><el-card shadow="hover" style="width: 90%;">
+        <el-row :gutter="0" v-loading="loading"><el-col :span="12"><el-card shadow="hover" style="width: 90%;">
               <div style="width: 100%; display: inline-block; text-align: center;">
                 <el-image style="width:85%; " id="result_img" :src="url" :fit="fill"></el-image>
               </div>
@@ -31,13 +31,14 @@
           <el-col :span="11">
             <el-card shadow="hover" style="width: 100%;">
               <div style="width: 100%; display: inline-block; ">
-                <el-statistic group-separator="," :precision="2" decimal-separator="." :value="value1" title="CLIP Score">
+                <el-statistic group-separator="," :precision="2" decimal-separator="." :value="clip_score"
+                  title="CLIP Score">
                 </el-statistic>
               </div>
             </el-card>
             <el-card shadow="hover" style="width: 100%;margin-top: 20px; ">
               <div style="width: 100%; display: inline-block; ">
-                <el-statistic :value="gen_time" title="生成用时(s)">
+                <el-statistic :value="gen_time" :precision="2" title="生成用时(s)">
                 </el-statistic>
               </div>
             </el-card><el-card shadow="hover" style="width: 100%;margin-top: 20px; ">
@@ -47,54 +48,6 @@
               </div>
             </el-card>
           </el-col></el-row>
-        <!--div>
-          <el-row :gutter="20">
-            <el-col :span="14">
-              <el-card shadow="hover" style="width: 100%;">
-                <div style="width: 100%; display: inline-block; ">
-                  <el-statistic :value="deadline2" time-indices title="🎉商品降价🎉">
-                    <template slot="suffix">
-                      抢购即将开始
-                    </template>
-                  </el-statistic>
-                </div>
-              </el-card>
-              <el-card shadow="hover" style="width: 100%;margin-top: 20px; ">
-                <div style="width: 100%; display: inline-block; ">
-                  <el-statistic @finish="hilarity" :value="deadline3" time-indices title="时间游戏">
-                    <template slot="suffix">
-                      <el-button type="primary " size="mini" @click="add">add 10s</el-button>
-                    </template>
-                  </el-statistic>
-                </div>
-              </el-card>
-              <el-card shadow="hover" style="width: 100%;margin-top: 20px; ">
-                <div style="width: 100%; display: inline-block;">
-                  <el-statistic format="DD天HH小时mm分钟" :value="deadline5" time-indices title="🚩距离立夏还有：">
-                  </el-statistic>
-                </div>
-              </el-card>
-            </el-col>
-            <el-col :span="10">
-              <el-card shadow="hover" style="width: 100%;">
-                <div slot="header" class="clearfix">
-                  <span>文嘉《明日歌》</span>
-                  <el-button style="float: right; padding: 3px 0" type="text" @click="clickFn">暂停</el-button>
-                </div>
-                <div style="font-size: 18px;text-align: center; margin-top: 35px;">
-                  明日复明日
-                </div>
-                <div style="font-size: 18px;text-align: center;">明日何其多</div>
-                <div style="font-size: 18px;text-align: center;">我生待明日</div>
-                <div style="font-size: 18px;text-align: center;">万事成蹉跎</div>
-                <div style="margin-top: 35px;"></div>
-                <el-statistic ref="statistic" @finish="hilarity" format="HH:mm:ss" :value="deadline4" title="距离明日："
-                  time-indices>
-                </el-statistic>
-              </el-card>
-            </el-col>
-          </el-row>
-        </div-->
       </el-tab-pane>
       <el-tab-pane label="结果对比" name="second">
         <el-select v-model="value" placeholder="请选择">
@@ -107,11 +60,17 @@
 </template>
 
 <script>
-//import HelloWorld from './components/HelloWorld.vue'
+import { Loading } from 'element-ui';
+
 
 const ipcRenderer = require('electron').ipcRenderer;
-ipcRenderer.on('cs-reply', (e, msg) => {
-  console.log("hi")
+var id = 0;
+var recent_msg;
+var msg_rcvd = false;
+ipcRenderer.on('generated-reply', (e, msg) => {
+  console.log(msg);
+  recent_msg = JSON.parse(msg);
+  msg_rcvd = true;
 });
 
 export default {
@@ -123,28 +82,31 @@ export default {
       activeName: 'main',
       select: 'en',
       gen_time: 3.24,
-      num: '50',
-      value1: 27.855,
+      iter_num: 50,
+      clip_score: 27.855,
       input_text: '',
       r_pre: 0.5,
       url: require('./assets/test.png')
     }
   },
   methods: {
-    start() {
-      ipcRenderer.send(this.input_text)
-      var x = 1
-      if (1 == x) {
-        ipcRenderer.send('open-child-now');
-      } else {
-        ipcRenderer.send('kill-child-now');
-      }
+    inputClick() {
+      ipcRenderer.send('text-input', [this.input_text, this.iter_num]);
+      this.timer = setInterval(this.checkResult, 1000);
     },
     handleClick() {
 
     },
+    checkResult() {
+      if (msg_rcvd) {
+        this.url = recent_msg.img_path //"data:image/jpg;base64," + recent_msg.img_path
+        this.clip_score = recent_msg.clip_score;
+        this.gen_time = recent_msg.time;
+        msg_rcvd = false;
+        clearInterval(this.timer);
+      }
+    },
     mounted() {
-      //this.url = '@/assets/test.png';
     },
   }
 }
--- src/background.js
+++ src/background.js
@@ -6,65 +6,43 @@ import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
 import ChildProcess from 'child_process'
 import YAML from 'yamljs'
-import iconv from 'iconv'
 const isDevelopment = process.env.NODE_ENV !== 'production'
 const ipcMain = require('electron').ipcMain;
 //const yaml = require("yamljs")
 const path = require("path")
-// 打开一个子进程
-const myChildProccess = require('child_process');
+
 var mySpawn = [];
-// 收到消息, 打开进程
-function handleGenerateRequest(e, msg) {
-  console.log(process.env.NODE_ENV)
-  const configPath = process.env.NODE_ENV === 'development' ? path.join(__dirname, '../src/assets/config.yaml') : path.join(process.cwd(), 'config.yml');
+function handleTextInput(e, msg) {
+  console.log(msg);
+  const configPath = process.env.NODE_ENV === 'development' ? path.join(__dirname, '../src/assets/config.yaml') : path.join(process.cwd(), 'config.yaml');
   const configFile = fs.readFileSync(configPath, 'utf-8');
   const config = YAML.parse(configFile);
-  console.log(e);
-  console.log(msg);
-  const execStr = `${config['python_path']} ${config['t2p_path']}/main.py --text ${msg}`
+  const execStr = `${config['python_path']} ${config['t2p_path']}/main.py --text "${msg[0]}" --iterations ${msg[1]}`
   console.log(execStr)
-  ChildProcess.exec('cd ' + config['t2p_path'] + ' ; ' + config['python_path'] + ' ' + config['t2p_path'] + 'main.py', {
+  ChildProcess.exec(execStr, {
     cwd: config['t2p_path']
   }, (error, stdout, stderr) => {
-    // if (error) {
-    //   console.error('error:', error);
-    //   return;
-    // }
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + iconv);
+    try {
+      var obj = JSON.parse(stdout);
+      if (obj != null) {
+        console.log(obj);
+        e.sender.send('generated-reply', JSON.stringify(obj));
+      }
+    } catch (error) {
+      console.log(error.toString())
+    }
   })
-  // var python = ChildProcess.spawn(config['python_path'], [config['t2p_path'] + '/demos/fp_optimization.py'], {
-  //   cwd: config['t2p_path']
-  // })
-  // python.stdout.on('data', (data) => {
-  //   console.log(`子进程输出:' ${data}`)
-  // });
-  // python.stderr.on('data', (data) => {
-  //   console.log(`子进程error:' ${data}`)
-  // });
-  // python.on('exit', (code, signal) => {
-  //   console.log('子进程退出:', `code ${code} and signal ${signal}`)
-  // });
-  e.sender.send('cs-reply', '打开进程:' + msg);
 }
-ipcMain.on('open-child-now', handleGenerateRequest); //(e, msg) => {
-//   console.log('打开进程-->mainProcessGet:' + msg)
-//   //var spawn = myChildProccess.spawn("C:\\Windows\\System32\\notepad.exe");// C:/Users/nickt/Desktop/1.py");//'C:\\Program Files\\Notepad++\\notepad++.exe'
-//   mySpawn[mySpawn.length] = spawn;
-//   handleGenerateRequest();
-//   e.sender.send('cs-reply', '打开进程:' + msg);
-// });
 
-ipcMain.on('kill-child-now', (e, msg) => {
-  console.log('关闭进程-->mainProcessGet:' + msg)
-  e.sender.send('cs-reply', '正在关闭所有打开的应用')
-  // 收到消息, 关闭所有进程
-  for (var i = 0; i < mySpawn.length; i++) {
-    mySpawn[i].kill();
-  }
-  mySpawn = [];
-});
+ipcMain.on('text-input', handleTextInput);
+//   console.log('关闭进程-->mainProcessGet:' + msg)
+//   e.sender.send('cs-reply', '正在关闭所有打开的应用')
+//   // 收到消息, 关闭所有进程
+//   for (var i = 0; i < mySpawn.length; i++) {
+//     mySpawn[i].kill();
+//   }
+//   mySpawn = [];
+// });
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
   { scheme: 'app', privileges: { secure: true, standard: true } }
@@ -76,7 +54,8 @@ async function createWindow() {
     width: 800,
     height: 720,
     webPreferences: {
-
+      webSecurity: false,
+      //preload: path.join(__dirname, '/src/preload.js'),
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
       nodeIntegration: true,//process.env.ELECTRON_NODE_INTEGRATION,
@@ -87,7 +66,7 @@ async function createWindow() {
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
     await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
-    if (!process.env.IS_TEST) win.webContents.openDevTools()
+    if (!process.env.IS_TEST);//win.webContents.openDevTools()
   } else {
     createProtocol('app')
     // Load the index.html when not in development
@@ -122,6 +101,7 @@ app.on('ready', async () => {
       console.error('Vue Devtools failed to install:', e.toString())
     }
   }
+  ipcMain.handle('text-input', handleTextInput)
   createWindow()
 })
 
